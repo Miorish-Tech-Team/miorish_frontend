4b8b6f041ac3a8e7ce2cbef9dfce6c67ac284a0a
Satish Singh <i.sksingh113@gmail.com>
2025-12-31 20:17:33 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.jpg" type="image/jpeg" />
+        <title>Miorish</title>
+      </head>
       <body
         className={`${cinzel.variable} ${inter.variable} antialiased`}
       >
