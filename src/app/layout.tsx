2972d439e7a6e2f5188f8dacb6cb21d0fde1ab6c
Satish Singh <i.sksingh113@gmail.com>
2026-01-01 14:35:04 +0530

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,24 +40,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GT-MB6DSH22');
-            `,
-          }}
-        />
-        {/* Google Analytics */}
+        {/* Google tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-TDK74C45P6"
+          src="https://www.googletagmanager.com/gtag/js?id=G-NDLS3PTR15"
         />
         <Script
           id="google-analytics"
@@ -67,21 +53,12 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-TDK74C45P6');
+              gtag('config', 'G-NDLS3PTR15');
             `,
           }}
         />
       </head>
       <body className={`${cinzel.variable} ${inter.variable} antialiased`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GT-MB6DSH22"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <AuthProvider>
           <CartProvider>
             <AuthModalProvider>
